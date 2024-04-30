7891fbf9085bb5f01763921b92069dc4aa3b3923
Taranko Taras <taras.taranko@intenselab.com>
2024-05-01 01:27:45 +0300

    Add importing from file

---
+++ SkiResort/src/Frontend/SkiResort.Frontend.Presentation/wwwroot/js/fileImporter.js
@@ -0,0 +1,13 @@
+﻿function uploadFile() {
+    return new Promise((resolve) => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = e => {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result);
+            reader.readAsText(file);
+        }
+        input.click();
+    });
+}
